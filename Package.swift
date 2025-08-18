2001d904a8a641cd2bad8a55267eab331f980648
Fernando V. Rodas R. <fernandorodas@protonmail.com>
2025-08-18 11:40:09 -0300

    Release/v1.0.18 patch1 (#4)
    * Preparing patch for release.
    
    * Checksum was updated

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
      targets: [
         .binaryTarget(
             name: "DigiyoSwiftPackage",
-            url: "https://github.com/digiyoid/identiasdk-ios/releases/download/v1.0.18/DigiyoSwiftPackage.zip",
-            checksum: "a448a9f5c7612bd716d409ee1d3e8c4a00812e61cee6a9a849adc29c4f2dc43c",
+            url: "https://github.com/digiyoid/identiasdk-ios/releases/download/v1.0.18-patch1/DigiyoSwiftPackage.zip",
+            checksum: "2b8f7c3650f61fcfbe767acca70f04c6c6f11c24450d6c8c89464e82d1a9ed27",
         )
     ]
 )
