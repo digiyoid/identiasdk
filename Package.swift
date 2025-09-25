7eaaa047d636c71c59dee3b0697f7348935a0dbe
Fernando V. Rodas R. <fernandorodas@protonmail.com>
2025-09-25 17:57:57 -0300

    Package.swift incorrect syntax fixed

--- Package.swift
+++ Package.swift
@@ -10,13 +10,13 @@ let package = Package(
         .library(
             name: "DigiyoSwiftPackage",
             targets: ["DigiyoSwiftPackage"]
-        ),
+        )
     ],
      targets: [
         .binaryTarget(
             name: "DigiyoSwiftPackage",
             url: "https://github.com/digiyoid/identiasdk/releases/download/v1.2.1/DigiyoSwiftPackage.xcframework.zip",
-            checksum: "b27df6bb7791298d18c075a0cbbb1a1f327049fd2327feb252efe07b80e84320",
+            checksum: "b27df6bb7791298d18c075a0cbbb1a1f327049fd2327feb252efe07b80e84320"
         )
     ]
 )
