418093c32285126a206faa29b4f078ad9bc11c93
Aurelio Almirón <aurelio.almiron@witec.id>
2026-07-30 13:19:55 -0300

    Preparing v1.5.1

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
         .binaryTarget(
             name: "DigiyoSwiftPackage",
             url:
-                "https://github.com/digiyoid/identiasdk/releases/download/v1.5.0/DigiyoSwiftPackage.xcframework.zip",
-            checksum: "2b7eda6a147b917f3a76f80a9c913ca2173e50db3df37f60f39a01ae768a043b"
+                "https://github.com/digiyoid/identiasdk/releases/download/v1.5.1/DigiyoSwiftPackage.xcframework.zip",
+            checksum: "7bb38389724c47f081da2f917b89ecf3aff6c362c2e95b7414eefa04401303c0"
         )
     ]
 )
